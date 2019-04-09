da8beb8e3e57012cb8ef8051a576a9f3a79aa2ec
metal-chaos <nightrage.metal66@gmail.com>
2019-04-09 15:55:35 +0900

    added polls.apps.PollsConfig to setting.py and applied migrations

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', #追加した箇所
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

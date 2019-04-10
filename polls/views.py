391be7e212f7ac7013480e656b9f5d8d72e67b65
metal-chaos <nightrage.metal66@gmail.com>
2019-04-10 14:52:50 +0900

    added polls template and modified views.py to indicate template file

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader # テンプレートファイルを読み出すものっぽい
 
-# Create your views here.
+from .models import Question # モデルからQuestionクラスを読み込み
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5] #配列で格納
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
